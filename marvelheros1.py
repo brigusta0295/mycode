b4e333b0f5abd11c4db241b42e4fb899554f2d10
Brian Gustafson <brigusta21@gmail.com>
2021-04-26 20:52:43 +0000

    learning about dictionaries

---
+++ marvelheros1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+marvel_heroes=["spiderman","Ironman","Hulk"]
+
+#Spiderman
+#Strength: super strong
+#Speed: greasy quick
+#Power: sticky fingers
+
+spiderman={"Strength": "super strong", "Speed": "greasy quick", "Power": "sticky fingers"}
+
+print(spiderman["Strength"])
